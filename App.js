04b9cff86ea78e7ec3754322a41a142e0da6ea7a
feiqiong-deng <feiqiong.deng@outlook.com>
2022-06-22 22:13:49 -0400

    Changes in style

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Hello there!");
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Tap to change"
-        onPress={() => setOutputText("Have a great day!")}
-      />
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View>
+        <TextInput
+          placeholder="Type here!"
+          style={{
+            height: 40,
+            borderColor: "blue",
+            borderWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
